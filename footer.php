b020968731ff0bf75b0c05a058166196d1528316
Zakia Afrin Jeme <zjeme@techlaunch.io>
2019-01-31 20:59:41 -0500

    first commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+<p>CopyRight &copy; <?php echo date("Y"); ?>. This site is created by Zakia Afrin Jeme. All Rights Reserved.</p>
+    </footer>
+    <?php wp_footer(); ?>
+  </body>
+</html>
